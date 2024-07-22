553fd692c9ed5e2dc2c10e1b765ad1e95e6edd67
araujomigueel <araujo.miguel.medeiros@gmail.com>
2024-07-22 13:29:55 -0300

    script lista block ok

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-/* var bookAddress = [];
+const viewArea = document.querySelectorAll(".bookLinks");
 
-function getAddresses(){
-    const newEmbedItem = document.createElement("embed");
-    newEmbedItem.src = bookAddress[0];
-    //booksarea.insertAdjacentElement("afterbegin", newEmbedItem);
-    
-    console.log(bookAddress[0].statusCode);
-}
-
-function insertBooks(){
-    const booksarea = document.getElementById("booksarea");
-    const newLink = document.createElement("a");
-    const newEmbedItem = document.createElement("embed");
-    
-    newLink.target = "_blank";
-    newEmbedItem.class = "book";
-
-    newLink.appendChild(newEmbedItem);
-    
-    for(var i = 0; i < 10; i++)
+function changeView(){
+    var isBlock = true;
+    if(viewArea[0].style.display == 'block')
     {
-        bookAddress[i] = (i+1);
-
-        newLink.href = `./assets/books/${bookAddress[i]}.pdf`;
-        newEmbedItem.src = newLink.href;
-
-        console.log(bookAddress[i]);
-        console.log(newLink.href);
-
-        booksarea.insertAdjacentElement("afterbegin", newLink);
+        isBlock = true;
     }
-} */
+    else
+    {
+        isBlock = false;
+    }
+    
+    for (let i = 0; i < viewArea.length; i++) {
+
+        viewArea[i].style.display = isBlock ? 'inline-block' : 'block';
+        
+    }
+    
+    
+    console.log(viewArea[0].style.display);
+    console.log(isBlock);
+}
